5a164a34be40840c528bcc98ae89a00beb3f437f
AIMMY-HOMEPC\noppo <noppon_aim@hotmail.com>
2021-08-30 12:48:25 +0700

    Add insertRecord.

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Phone;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    // insertRecord
+    public function insertRecord() {
+        $phone = new Phone();
+        $phone->phone = "1234567890";
+        $user = new User();
+        $user->name = "Jennifer";
+        $user->email = "jennifer@gmail.com";
+        $user->password = encrypt('secret');
+        $user->save();
+        $user->phone()->save($phone);
+        return "Record has been create successfully!";
+    }
+}
