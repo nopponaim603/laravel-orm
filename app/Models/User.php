d4b5fb44a1a672e68340ebe0b401de86678f3437
AIMMY-HOMEPC\noppo <noppon_aim@hotmail.com>
2021-08-31 10:39:51 +0700

    create Model and relation.

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
     public function phone(){
         return $this->hasOne('App\Models\Phone');
     }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class,'role_users');
+    }
 }
